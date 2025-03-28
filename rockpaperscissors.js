3d4de87dc4592a3fb9a95a3abc59ec7c6f6f694f
beaverman3426 <hoibeaver@gmail.com>
2025-03-28 23:32:17 +0200

    rock paper scissors using DOM and events

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -5,3 +5,38 @@ let computerScore = 0;
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
+function determineWinner(human, computer) {
+  const outcomes = {
+      rock: { scissors: "human", paper: "computer", rock: "draw" },
+      scissors: { paper: "human", rock: "computer", scissors: "draw" },
+      paper: { rock: "human", scissors: "computer", paper: "draw" }
+  };
+
+  return outcomes[human][computer];
+}
+function updateUI(humanChoice, computerChoice, result) {
+  document.getElementById("result").textContent =
+      `You chose ${humanChoice}, Computer chose ${computerChoice}. ${result}`;
+
+  document.getElementById("humanScore").textContent = humanScore;
+  document.getElementById("computerScore").textContent = computerScore;
+}
+document.querySelectorAll(".choices button").forEach(button => {
+  button.addEventListener("click", function () {
+      let humanChoice = this.id;
+      let computerChoice = getComputerChoice();
+      let result = determineWinner(humanChoice, computerChoice);
+
+      if (result === "human") {
+          humanScore++;
+          result = "You win!";
+      } else if (result === "computer") {
+          computerScore++;
+          result = "You lose!";
+      } else {
+          result = "It's a draw!";
+      }
+
+      updateUI(humanChoice, computerChoice, result);
+  });
+});
